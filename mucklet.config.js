d0abe9f52923dd10806062ef6b7ed5f73299e2e1
Accipiter Nisus <accipiter@gmail.com>
2026-05-08 13:02:00 +0200

    Updated documentation in mucklet.config.js file.

--- mucklet.config.js
+++ mucklet.config.js
@@ -1,7 +1,17 @@
 const config = {
 	realm: {
 		/** URL to the realm API WebSocket endpoint. */
-		apiUrl: 'wss://api.test.mucklet.com',
+		// apiUrl: 'wss://api.test.mucklet.com',
+
+		/**
+		 * For security reason, it is not possible to store the token in the config file.
+		 *
+		 * Instead consider using:
+		 * - MUCKLET_BOT_TOKEN_FILE environment variable with the path to a file containing the token
+		 * - MUCKLET_BOT_TOKEN environment variable containing the token
+		 * - --tokenfile flag with a path to a file containing the token
+		 * - --token flag with the token.
+		 */
 	},
 };
 
